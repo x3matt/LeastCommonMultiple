774675a79f7051c434985a4eb745714bccbc7d46
x3matt <x3matt21@mail.ru>
2019-10-10 00:29:50 +0700

    edited code

--- src/LCN.java
+++ src/LCN.java
@@ -1,8 +1,8 @@
-public class LMN {
+public class LCN {
     public int gcd(int a,int b){
         return b==0?a:gcd(b,a%b);
     }
-    public int lmn(int a,int b){
+    public int lcn(int a,int b){
         return (a*b)/gcd(a,b);
     }
 }
--- src/TestClass.java
+++ src/TestClass.java
@@ -3,12 +3,12 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class TestClass {
-    LMN lmn = new LMN();
+    LCN lmn = new LCN();
     @Test
     public void testLMN(){
-        assertEquals(15,lmn.lmn(3,5));
-        assertEquals(12,lmn.lmn(12,12));
-        assertEquals(140,lmn.lmn(35,4));
-        assertEquals(110,lmn.lmn(10,22));
+        assertEquals(15,lmn.lcn(3,5));
+        assertEquals(12,lmn.lcn(12,12));
+        assertEquals(140,lmn.lcn(35,4));
+        assertEquals(110,lmn.lcn(10,22));
     }
 }
