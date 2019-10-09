a4ed65ff03b79f430967539a52526c3b980df6c9
x3matt <x3matt21@mail.ru>
2019-10-10 00:39:06 +0700

    edited code

--- src/LCM.java
+++ src/LCM.java
@@ -2,7 +2,7 @@ public class LCM {
     public int gcd(int a,int b){
         return b==0?a:gcd(b,a%b);
     }
-    public int lcn(int a,int b){
+    public int lcm(int a,int b){
         return (a*b)/gcd(a,b);
     }
 }
--- src/TestClass.java
+++ src/TestClass.java
@@ -6,9 +6,9 @@ public class TestClass {
     LCM lcm = new LCM();
     @Test
     public void testLMN(){
-        assertEquals(15,lcm.lcn(3,5));
-        assertEquals(12,lcm.lcn(12,12));
-        assertEquals(140,lcm.lcn(35,4));
-        assertEquals(110,lcm.lcn(10,22));
+        assertEquals(15,lcm.lcm(3,5));
+        assertEquals(12,lcm.lcm(12,12));
+        assertEquals(140,lcm.lcm(35,4));
+        assertEquals(110,lcm.lcm(10,22));
     }
 }
